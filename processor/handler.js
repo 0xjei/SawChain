614e8efff6b0eabd0ce36213c468f2a38f7cbee7
Giacomo Corrias <giacomo.corrias7@gmail.com>
2020-01-03 01:31:11 +0100

    [feat] add processor registration to validator

--- processor/handler.js
+++ processor/handler.js
@@ -1,1 +1,38 @@
-// Transaction Processor Handler.
+'use strict'
+
+const { TransactionHandler } = require('sawtooth-sdk/processor/handler')
+const { InvalidTransaction } = require('sawtooth-sdk/processor/exceptions')
+
+const FAMILY_NAME = 'agrichain'
+const FAMILY_VERSION = '0.1'
+const NAMESPACE = 'bc3a20'
+
+/**
+ * Extension of TransactionHandler class for the AgriChain Transaction Processor logic.
+ */
+class AgriChainHandler extends TransactionHandler {
+  /**
+   * TransactionHandler constructor registers itself with the
+   * validator, declaring which family name, versions, and
+   * namespaces it expects to handle.
+   */
+  constructor () {
+    super(FAMILY_NAME, [FAMILY_VERSION], [NAMESPACE])
+  }
+
+  /**
+   * Smart contract logic core. It'll be called once for every transaction.
+   * Validate each action logic and update state according to it.
+   * @param {Transaction} txn Transaction process request.
+   * @param {Context} context Current state context.
+   */
+  async apply (txn, context) {
+    let payload = null
+    // todo
+    // Payload decoding.
+    // Get action.
+    // Action handling.
+  }
+}
+
+module.exports = AgriChainHandler
--- processor/index.js
+++ processor/index.js
@@ -1,1 +1,12 @@
-// JS Entry poin file.
+'use strict'
+
+const { TransactionProcessor } = require('sawtooth-sdk/processor')
+
+const AgriChainHandler = require('./handler')
+
+const tp = new TransactionProcessor(process.env.VALIDATOR_URL || 'tcp://localhost:4004')
+const handler = new AgriChainHandler()
+
+// Add the handler and start the transaction processor.
+tp.addHandler(handler)
+tp.start()
